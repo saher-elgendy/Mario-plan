86d806e158478dee6dc97ce823783c6b4b6c1496
unknown <saherelgendy1990@gmail.com>
2020-07-03 17:46:26 -0700

    creating Navbar component

---
+++ src/components/Layout/Navbar.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                <Link className="brand-logo" to="/">
+                    Mario plan
+                </Link>
+            </div>
+            <SignedInLinks />
+            <SignedOutLinks />
+        </nav>
+    )
+}
+
+export default Navbar
