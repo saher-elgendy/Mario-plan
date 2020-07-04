6913595477061b0c9d0fbe151723a50617bb01fd
unknown <saherelgendy1990@gmail.com>
2020-07-04 12:22:28 -0700

    connecting Dashboard component to store

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ProjectList from '../Projects/ProjectList';
 import Notifications from './Notifications';
 
-function Dashboard() {
+function Dashboard(props) {
+    console.log(props)
     return (
         <div className="dashboard container">
             <div className="row">
                 <div className="col s12 m6">
-                    <ProjectList />
+                    <ProjectList projects={props.projects}/>
                 </div>
                 <div className="col s12 m5 offset-ml">
                     <Notifications />
@@ -17,4 +19,10 @@ function Dashboard() {
     )
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+   return {
+       projects: state.projects.projects
+   }
+}
+
+export default connect(mapStateToProps)(Dashboard);
