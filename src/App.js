bb8786db06e0f1b307af6333c2bb569a3ca1b176
unknown <saherelgendy1990@gmail.com>
2020-07-03 17:43:48 -0700

    create project routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
+import Dashboard from './components/Dashboard/Dashboard';
+import Navbar from './components/Layout/Navbar';
+import CreateProjects from './components/Projects/CreateProjects';
+import ProjectDetails from './components/Projects/ProjectDetails';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProjects} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
