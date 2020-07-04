6b878beda796e929b8f36da52e32cb3a22fdc1c8
unknown <saherelgendy1990@gmail.com>
2020-07-04 12:27:57 -0700

    using project prop fields  in ProjectSummary

--- src/components/Projects/ProjectSummary.js
+++ src/components/Projects/ProjectSummary.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ p }) => {
+const {title, content} = p;
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted By Saher Elgendy</p>
+                <span className="card-title">{title}</span>
+                <p>{content}</p>
                 <p className="grey-text">3rd July, 2020</p>
             </div>
         </div>
