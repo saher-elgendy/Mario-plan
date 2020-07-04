147b84c3ba3aa2d11e47cc496d754bafd77b5a55
unknown <saherelgendy1990@gmail.com>
2020-07-04 12:21:28 -0700

    linking redux and firebase to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+import firebase from 'firebase/app';
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { getFirebase, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { createFirestoreInstance, getFireStore, reduxFirestore } from 'redux-firestore';
+import thunk from 'redux-thunk';
 import App from './App';
+import fbConfig from './config/fbConfig';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFireStore })),
+    reduxFirestore(firebase, fbConfig)
+  )
+);
+
+const rrfConfig = {
+	userProfile: 'users',
+	useFirestoreForProfile: true,
+	attachAuthIsReady: true
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
