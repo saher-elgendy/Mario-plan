913fda1f665ecd858da8dccb39381ab75c0e4cb6
unknown <saherelgendy1990@gmail.com>
2020-07-04 21:48:13 -0700

    creating createProject action

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,8 +1,15 @@
 export const createProject = (project) => {
-    return (dispatch, getState, {getFirebase, getFirestore}) => {
-        dispatch({
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firestore = getFirestore();
+
+        firestore.collection('projects').add({
+            ...project
+        }).then(() => dispatch({
             type: 'CREATE_PROJECT',
             project
-        })
+        })).catch(err => dispatch({
+            type: 'CREATE_ERROR',
+            err
+        }))
     }
 }
