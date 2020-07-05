0936485224b1fe91980742cb7372bd634531fcce
unknown <saherelgendy1990@gmail.com>
2020-07-04 21:46:52 -0700

    deploying project to firebase

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import ProjectList from '../Projects/ProjectList';
 import Notifications from './Notifications';
 
@@ -9,7 +11,7 @@ function Dashboard(props) {
         <div className="dashboard container">
             <div className="row">
                 <div className="col s12 m6">
-                    <ProjectList projects={props.projects}/>
+                    <ProjectList projects={props.projects} />
                 </div>
                 <div className="col s12 m5 offset-ml">
                     <Notifications />
@@ -20,9 +22,13 @@ function Dashboard(props) {
 }
 
 const mapStateToProps = (state) => {
-   return {
-       projects: state.projects.projects
-   }
+    console.log(state)
+    return {
+        projects: state.firestore.ordered.projects || []
+    }
 }
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([{ collection: 'projects' }])
+)(Dashboard);
