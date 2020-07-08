034e5a12f0f20335996b99c334089b9b2762a11f
unknown <saherelgendy1990@gmail.com>
2020-07-07 18:03:12 -0700

    adding 'SIGNUP_SUCCESS' to auth reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -2,22 +2,35 @@
 const initialState = {
     authErr: null
 }
+
 export default (state = initialState, action) => {
     switch (action.type) {
-        case 'SIGN_IN_SUCCESS':
+        case 'SIGNIN_SUCCESS':
             console.log('sign in success');
             return {
                 ...state, authErr: null
             }
-        case 'SIGN_IN_ERROR': 
+        case 'SIGNIN_ERROR':
             console.log('sign in error');
             return {
                 ...state,
                 authErr: 'Sign in failed'
             }
-            case 'SIGN_OUT_SUCCESS':
-                console.log('sign out success')
-                return state;
+        case 'SIGNOUT_SUCCESS':
+            console.log('sign out success')
+            return state;
+        case 'SIGNUP_SUCCESS':
+            console.log('sign up success')
+            return {
+                ...state,
+                authErr: null
+            }
+        case 'SIGNUP_ERROR':
+            console.log('sign up failed')
+            return {
+                ...state,
+                authErr: action.err.message
+            }
         default:
             return state;
     }
