74a1f882c9b2fa75d17b994831240b15df3c49b4
unknown <saherelgendy1990@gmail.com>
2020-07-07 18:03:33 -0700

    removing dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,17 +1,10 @@
-const initialstate = {
-    projects: [
-        { id: 1, title: 'project1', content: 'bla bla bla' },
-        { id: 2, title: 'project2', content: 'bla bla bla' },
-        { id: 2, title: 'project3', content: 'bla bla bla' },
-    ]
-}
-export default (state = initialstate, action) => {
+
+export default (state = {}, action) => {
     switch(action.type) {
         case 'CREATE_PROJECT':
             console.log(action.project);
             return state;
             case 'CREATE_ERROR':
-                console.log(action.err);
                 return state;
             default:
                 return state;
