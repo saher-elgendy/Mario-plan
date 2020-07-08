42e1bcb6b42fe25a15aea0877bdb234707d70ae1
unknown <saherelgendy1990@gmail.com>
2020-07-07 18:01:04 -0700

    adding signUp action

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,12 +1,13 @@
+
 //sign in action
 export const signIn = ({ email, password }) => {
     return (dispatch, getState, { getFirebase }) => {
         const firebase = getFirebase();
 
         firebase.auth().signInWithEmailAndPassword(email, password).then(() => dispatch({
-            type: 'SIGN_IN_SUCCESS'
+            type: 'SIGNIN_SUCCESS'
         })).catch(err => dispatch({
-            type: 'SIGN_IN_ERROR',
+            type: 'SIGNIN_ERROR',
             err
         }))
     }
@@ -18,9 +19,34 @@ export const signOut = () => {
         const firebase = getFirebase();
 
         firebase.auth().signOut().then(() => dispatch({
-            type: 'SIGN_OUT_SUCCESS'
+            type: 'SIGNOUT_SUCCESS'
         }))
     }
 }
 
 //sign up action
+
+export const signUp = ({ email, password, firstName, lastName }) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+        
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then((resp) => {
+                return firestore.collection('users').doc(resp.user.uid).set({
+                    firstName: firstName,
+                    lastName: lastName,
+                    initials: firstName[0] + lastName[0]
+                });
+            })
+            .then(() => {
+                dispatch({ type: 'SIGNUP_SUCCESS' });
+            })
+            .catch((err) => {
+                dispatch({ type: 'SIGNUP_ERROR', err });
+            });
+    };
+
+}
