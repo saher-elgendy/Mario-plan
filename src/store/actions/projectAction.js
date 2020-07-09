0d14361dc5ab465f3dc9c84264767f0d0147ce08
unknown <saherelgendy1990@gmail.com>
2020-07-09 12:32:47 -0700

    adding author first name, last name and id to project

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,9 +1,16 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         const firestore = getFirestore();
+        const state = getState();
+        const profile = state.firebase.profile;
+        const authorId = state.firebase.auth.uid;
 
         firestore.collection('projects').add({
-            ...project
+            ...project,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
+            createdAt: new Date()
         }).then(() => dispatch({
             type: 'CREATE_PROJECT',
             project
