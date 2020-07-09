6c4a7bd4feaad949b0c2a579bcbd74188100aaca
unknown <saherelgendy1990@gmail.com>
2020-07-09 12:24:41 -0700

    using moment calendar as a project creation date

--- src/components/Projects/ProjectSummary.js
+++ src/components/Projects/ProjectSummary.js
@@ -1,14 +1,15 @@
+import moment from 'moment';
 import React from 'react';
 
 const ProjectSummary = ({ project }) => {
-const {title, content} = project;
+const {title, content, createdAt} = project;
 
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{title}</span>
                 <p>{content}</p>
-                <p className="grey-text">3rd July, 2020</p>
+                <p className="grey-text">{moment(createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     )
